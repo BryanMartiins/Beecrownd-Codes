cfb844f593a71a2f88c21baa5df054000b303fba
BryanMartiins <bryanmartins2309@hotmail.com>
2023-06-29 11:04:10 -0300

    Bee Solutions

--- Java/1132.java/Main.java
+++ Java/1132.java/Main.java
@@ -1,25 +1,32 @@
-
 import java.util.Scanner;
+
 public class Main {
-     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int X, Y,soma=0, aux;
+        int X, Y, soma = 0;
 
-        X= scanner.nextInt();
-        Y= scanner.nextInt();
+        X = scanner.nextInt();
+        Y = scanner.nextInt();
 
-        if (X>Y){
-            aux=Y;
-            Y=X;
-            X=aux;
+        if (X > Y) {
+            for (int i = Y; i <= X; i++) {
+                if (i % 13 != 0) {
+                    soma += i;
+                }
+            }
         }
-        for (int i = X; X<=Y;i++){
-            soma+=i;
+
+        else {
+            for (int i = X; i <= Y; i++) {
+                if (i % 13 != 0) {
+                    soma += i;
+                }
+            }
         }
 
         System.out.println(soma);
-
         
     }
 }
+
