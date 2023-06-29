72bc47a18abafc382dce62ea3b0e04534fc1c800
BryanMartiins <bryanmartins2309@hotmail.com>
2023-06-29 11:37:27 -0300

    Bee Solutions

---
+++ 1144.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int mult;
+        for (int i = 1; i <= N; i++) {
+            mult = i * i;
+
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+            System.out.println(i + " " + ((i * i) + 1) + " " + ((i * i * i) + 1));
+
+        }
+    }
+}
