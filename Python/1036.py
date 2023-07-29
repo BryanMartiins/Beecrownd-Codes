190b628d5d91847374a5acfaa51f51d13e7a5ba8
BryanMartiins <bryanmartins2309@hotmail.com>
2023-07-29 10:40:20 -0300

    Bee Solutions

--- Python/1036.py
+++ Python/1036.py
@@ -1,14 +1,18 @@
 a, b, c = list (map(float,input().split()))
 
-delta = (b * b ) - (4*a*c)
+delta = (b ** 2  - 4*a*c)
 
-r1 = float (-b+((delta**(1/2))/(2*a)))
-r2 = float (-b-((delta**(1/2))/(2*a)))
+
 
 if delta <= 0:
     print ("Impossivel calcular")
+    breakpoint
 elif a == 0:
     print ("Impossivel calcular")
+    breakpoint
 else:
-    print (f"R1 ={r1:.5f}")
-    print (f"R2 ={r2:.5f}")    
+
+    r1 = (-b+(delta**(1/2)))/(2*a)
+    r2 = (-b-(delta**(1/2)))/(2*a)
+    print (f"R1 = {r1:.5f}")
+    print (f"R2 = {r2:.5f}")    
