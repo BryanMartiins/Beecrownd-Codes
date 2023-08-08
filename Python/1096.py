e2bec41c06d7d9632ff19bad36e9caf55ffda711
BryanMartiins <bryanmartins2309@hotmail.com>
2023-08-08 09:47:30 -0300

    Bee Solutions

--- Python/1096.py
+++ Python/1096.py
@@ -1,11 +1,5 @@
-j = 7
-
-for i in range (0,9):
-    
-   
-        
-
-       
-        
-
-        
+for i in range (1,10,2):
+    j = 7
+    for K in range (3):
+        print(f"I={i} J={j}")
+        j-=1
---
+++ Python/1097.py
@@ -0,0 +1,9 @@
+i = 1 
+J = 7
+
+while  i <= 9:
+    print (f"I={i:d} J={J-1}")
+    print (f"I={i:d} J={J-1}")
+    print (f"I={i:d} J={J-1}")
+    i +=2
+    J +=5
